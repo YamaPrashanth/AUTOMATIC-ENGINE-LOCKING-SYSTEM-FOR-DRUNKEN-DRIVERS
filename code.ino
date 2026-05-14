58f5726939c23b1b017c8633245b1c8bff5cf46b
YamaPrashanth <yamaprashanth15@gmail.com>
2026-05-14 07:51:59 +0530

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,54 @@
+//alcohol engine locking
+#include <LiquidCrystal.h>
+
+// LCD pins: RS, EN, D4-D7
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+const int gassensor = A0;  // Analog pin for vibration sensor
+const int motorPin = 9;          // DC motor pin (via motor driver)
+const int buzzerPin = 8;         // Buzzer pin
+const int Threshold = 100; // Threshold for alcohol detection (adjust based on sensor calibration)
+
+void setup() {
+  pinMode(gassensor, INPUT);
+  pinMode(motorPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.clear();
+}
+
+void loop() {
+  int Gassensorlevel = analogRead(gassensor); // Read alcohol sensor value
+  
+  // Display alcohol sensor reading on LCD
+  lcd.setCursor(0, 0);
+  // int val=digitalRead(2);
+  if (Gassensorlevel>100)
+     {
+      lcd.print("Alcohol val");
+     lcd.setCursor(13,0);
+      lcd.print(Gassensorlevel);
+        lcd.setCursor(0,1);
+  lcd.print("engine Locked");
+     digitalWrite(motorPin, LOW); // Turn on the motor (engine running)
+    digitalWrite(buzzerPin, HIGH); // Turn off buzzer
+  }
+  else {
+  // if(val==HIGH)
+
+ lcd.print("Alcohol val");
+     lcd.setCursor(13,0);
+      lcd.print(Gassensorlevel);
+        lcd.setCursor(0,1);
+     lcd.print(" Safe");
+     Serial.println("READY TO GO");
+    digitalWrite(motorPin, HIGH); // Turn on the motor (engine running)
+    digitalWrite(buzzerPin, LOW); // Turn off buzzer
+  }
+
+  delay(500); // Small delay to stabilize sensor readings
+  lcd.clear();
+}
